60e6ed969e564390bc36cb0ffc7175a66397bc22
Shamim Imtiaz <km_si@ymail.com>
2021-03-02 14:46:37 -0500

    initial setup

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+//import the .env file
+require('dotenv').config();
+//// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
