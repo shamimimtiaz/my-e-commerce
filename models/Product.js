26e621936b693f658f24616e2d668ca74e0b080e
Shamim Imtiaz <km_si@ymail.com>
2021-03-06 14:44:56 -0500

    create routes for category, product and tag

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,5 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-const { Category } = require('.');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,51 +9,50 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-// define id column     
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-  },
-// define product column
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-  },
-// define price column
-    price: {
-      type: DataTypes.DECIMAL(10,3),
-      allowNull: false,
-      validate: {
-        isDecimal: true
+      // define an id column
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      // define product_name column
+      product_name: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      // define price column
+      price: {
+          type: DataTypes.DECIMAL(10,2),
+          allowNull: false,
+          validate: {
+              isDecimal: true
+          }
+      },
+      // define stock column
+      stock: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 10,
+          validate: {
+              isNumeric: true
+          }
+      },
+      // define category_id column
+      category_id: {
+          type: DataTypes.INTEGER,
+          references: {
+              model: "category",
+              key: "id"
+          }
       }
   },
-// define stock column
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-  },
-// define category_id column
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        //key: id
-      }
-    }
-  },
-
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'product',
   }
 );
 
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,36 +6,36 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-     // define product_id column
-     product_id: {
-       type: DataTypes.INTEGER,
-       references: {
-         model: 'product',
-         key: 'id'
-       }
-     },
-     //define tag_id column
-     tag_id: {
-       type: DataTypes.INTEGER,
-       references: {
-         model: tag,
-         key: id 
-       }
-     }
+      // define an id column
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      // define product_id column
+      product_id: {
+          type: DataTypes.INTEGER,
+          references: {
+              model: "product",
+              key: "id"
+          }
+      },
+      // define tag_id column
+      tag_id: {
+          type: DataTypes.INTEGER,
+          references: {
+              model: "tag",
+              key: "id"
+          }
+      }
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'product_tag',
   }
 );
 
